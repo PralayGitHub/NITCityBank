30aafc0dcd4e70a326778eb7ad40551527450d54
prola <prola@DESKTOP-SQ4K74F>
2020-04-16 00:38:34 +0530

    dev2 modified Test.java

--- DemoProj2/src/com/nt/test/test.java
+++ DemoProj2/src/com/nt/test/test.java
@@ -1,5 +1,8 @@
 package com.nt.test;
-
+import com.nt.beans.Utility;
 public class test {
-
+	public static void main(String args[]){
+		Utility u1=new Utility();
+		System.out.println(u1.getSysDate());
+	}
 }
